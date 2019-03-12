58199285a6853b91a72f1cacae1f329370625780
Etienne Sellan <git@sellan.fr>
2019-03-12 14:56:13 +0100

    Start structuration

--- tls.py
+++ tls.py
@@ -2,20 +2,22 @@
 
 from tkinter import *
 
-window = Tk()
-
-window.geometry("1000x500")
-
-panel = PanedWindow(window, orient=HORIZONTAL)
-panel.pack(side=LEFT, expand=True, fill=BOTH)
-
-panel.add(Frame(panel, borderwidth=2, relief=GROOVE))
-
-panel.add(Button(panel, width=25, text="Fermer", command=window.quit))
-
-panel.add(Label(panel, text='Exchange history', background='grey', anchor=CENTER))
-
-panel.pack()
+def prepare_gui_structure():
+  window = Tk()  # Create a window
+  window.geometry("1000x500")  # Define window size
+  window.title("TLS Learning Software")  # Define window title
+  # now, set a panel in the window
+  panel = PanedWindow(window, orient=HORIZONTAL)  # Add panel to window
+  panel.pack(side=LEFT, expand=True, fill=BOTH)  # Set the panel alignment to left
 
 
-window.mainloop()
+def add_permanent_elements():
+  panel.add(Button(panel, width=75, text="Fermer", command=window.quit))
+  panel.add(Label(panel, text='Exchange history', background='grey', anchor=CENTER))
+  panel.pack()
+
+
+if __name__ == "__main__":
+  prepare_gui_structure()
+  add_permanent_elements()
+  window.mainloop()
